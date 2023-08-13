b116c10f07ac42216587e60cdd54a8144ecdfca5
cjhornung <cjhornung@gmail.com>
2023-08-13 13:26:40 -0500

    Department table creation

--- EmployeeSQL/Table_Creation_Schema.sql
+++ EmployeeSQL/Table_Creation_Schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 select * from employees;
+
+-- Departments table creation
+CREATE TABLE departments (
+  dept_no VARCHAR(30) NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (dept_no)
+);
+
+select * from departments
