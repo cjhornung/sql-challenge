207e9b2c44ad48618a07cb2e4a6c14497e67f072
cjhornung <cjhornung@gmail.com>
2023-08-13 15:02:36 -0500

    Completed task no. 8

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -75,3 +75,8 @@ ON departments.dept_no=dept_emp.dept_no
 WHERE departments.dept_name='Sales' OR departments.dept_name='Development';
 
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
