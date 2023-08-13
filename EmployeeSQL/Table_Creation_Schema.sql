5fa739b845b672601c19299cfb96e9cd5a497514
cjhornung <cjhornung@gmail.com>
2023-08-13 14:03:50 -0500

    Titles table creation
    had to rearrange some of the table creation schema since titles table should be created before employees table

--- EmployeeSQL/Table_Creation_Schema.sql
+++ EmployeeSQL/Table_Creation_Schema.sql
@@ -1,7 +1,18 @@
 -- Table creation schema
 
+-- titles table creation
+DROP TABLE titles;
+
+CREATE TABLE titles (
+	title_id VARCHAR(30) NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+select * from titles;
+
 -- Employees table creation
-DROP TABLE employees
+DROP TABLE employees;
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
@@ -11,12 +22,13 @@ CREATE TABLE employees (
 	last_name VARCHAR(30) NOT NULL,
 	sex VARCHAR(1) NOT NULL,
 	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	PRIMARY KEY (emp_no)
 );
 select * from employees;
 
 -- Departments table creation
-DROP TABLE departments
+DROP TABLE departments;
 
 CREATE TABLE departments (
   dept_no VARCHAR(30) NOT NULL,
@@ -27,7 +39,7 @@ CREATE TABLE departments (
 select * from departments;
 
 -- dept_emp table creation 
-DROP TABLE dept_emp
+DROP TABLE dept_emp;
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
@@ -36,10 +48,10 @@ CREATE TABLE dept_emp (
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-select * from dept_emp
+select * from dept_emp;
 
 -- salaries table creation 
-DROP TABLE salaries
+DROP TABLE salaries;
 
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
@@ -47,11 +59,11 @@ CREATE TABLE salaries (
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
-select * from salaries
+select * from salaries;
 
 -- dept_managers table creation
 
-DROP TABLE dept_managers
+DROP TABLE dept_managers;
 
 CREATE TABLE dept_managers (
  dept_no VARCHAR (30),
@@ -60,6 +72,6 @@ CREATE TABLE dept_managers (
  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-select * from dept_managers
+select * from dept_managers;
 
 
