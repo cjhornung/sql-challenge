a40f049d52acc7ab9a87ee4ed407836a2d2bbf95
cjhornung <cjhornung@gmail.com>
2023-08-13 14:57:59 -0500

    Completed task no. 7

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -63,4 +63,15 @@ WHERE departments.dept_name='Sales';
 
 -- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM employees 
+JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales' OR departments.dept_name='Development';
+
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
